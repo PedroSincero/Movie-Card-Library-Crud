56d1536a7eb8b42cdbec35221abbd362691805e5
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-07 20:41:47 -0300

    Desafio 4 - COmplete :rocket:

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id" render={(props) => <MovieDetails { ...props } /> } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </BrowserRouter>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-
+import Link from 'react-router-dom/Link';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: '',
+      // loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { match:{ params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((movie) => {
+      this.setState({ movie });
+    });
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+  // Change the condition to check the state
+  // if (true) return <Loading />;
+    const { match:{ params: { id } } } = this.props;
+    const { movie: { title, storyline, imagePath, genre, rating, subtitle } } = this.state;
+    const { movie } = this.state;
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        {movie === '' ? <Loading />
+          : (
+            <div>
+              <img alt="Movie Cover" src={ `../${imagePath}` } />
+              <p>{ `title: ${title}` }</p>
+              <p>{ `Subtitle: ${subtitle}` }</p>
+              <p>{ `Storyline: ${storyline}` }</p>
+              <p>{ `Genre: ${genre}` }</p>
+              <p>{ `Rating: ${rating}` }</p>
+              <Link to="/">VOLTAR</Link>
+              <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+            </div>
+          )}
       </div>
     );
   }
