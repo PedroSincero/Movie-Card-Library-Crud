576ac96237f5ab04ab16f9d489b1dd0936b31eee
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-07 18:04:49 -0300

    adicionando ajustes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" component={ MovieList } />
+        <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
