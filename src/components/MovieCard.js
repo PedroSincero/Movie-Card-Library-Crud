8bd5127d19b1357a1cf9ae2dcc6ed7d9d3b91040
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-07 17:56:30 -0300

    adicionando Desafio 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Link from 'react-router-dom/Link';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { title, subtitle, storyline, imagePath, rating, id } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
         <div className="movie-card-body">
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
