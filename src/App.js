0d8d4b29f747ba1b5c7f77c5760e6024a4d49543
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-07 17:36:10 -0300

    Desafio 2 : Complete

--- src/App.js
+++ src/App.js
@@ -7,17 +7,25 @@ import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route component={ NotFound } />
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     movie: '',
+  //   };
+  // }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +9,28 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      // loading: true,
     };
+  }
+
+  // loadingStart() {
+  // }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((movie) => {
+      this.setState({ movies: movie });
+    });
   }
 
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
-
+    // if (loading === false) return ();
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {!movies.length ? <Loading />
+          : (movies.map((movie) => (<MovieCard key={ movie.title } movie={ movie } />)))}
       </div>
     );
   }
