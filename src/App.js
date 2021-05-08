52519379e708e00f947e577e7578bee8614d9ff5
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-05-08 17:24:56 -0300

    adicionando modificaçoes

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ class App extends React.Component {
             path="/movies/:id/edit"
             render={ (props) => <EditMovie { ...props } /> }
           />
-          <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
           <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
           <Route exact path="/" component={ MovieList } />
           <Route component={ NotFound } />
         </Switch>
